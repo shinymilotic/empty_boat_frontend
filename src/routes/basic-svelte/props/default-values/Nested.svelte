4391ffc44b5126b010b23789e2c70addf068d849
shinymilotic <trungtin.mai1412@gmail.com>
2026-05-23 09:17:36 +0700

    commit

---
+++ src/routes/basic-svelte/props/default-values/Nested.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    const {answer='Hello, World!', answerId = 3} = $props();
+</script>
+<div>The answer is {answer}, ID = {answerId}</div>
---
+++ src/routes/basic-svelte/props/spread-props/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Nested from "./Nested.svelte";
+
+    const char = {name : 'Mai', address:"Binh Thai", genre:"Female"};
+</script>
+
+<Nested {...char} ></Nested>
+
